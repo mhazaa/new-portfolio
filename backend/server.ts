55639fbdafbe54e3d7519b0f529abdb09b4eb2ff
mhazaa <magdihazaa@gmail.com>
2024-02-27 03:17:34 -0600

    updating port

--- backend/server.ts
+++ backend/server.ts
@@ -8,7 +8,7 @@ import router from './router';
 
 dotenv.config();
 
-const PORT = 80;
+const PORT = 3000;
 const app: express.Application = express();
 app.listen(PORT, () => console.log(`Listening to port: ${PORT}`));
 
