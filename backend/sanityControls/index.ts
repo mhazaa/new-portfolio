e9568276d3c8f198bc95d61950d5f4dfb1aa83b1
mhazaa <magdihazaa@gmail.com>
2024-05-07 05:15:46 -0500

    removing unused export

--- backend/sanityControls/index.ts
+++ backend/sanityControls/index.ts
@@ -3,7 +3,7 @@ import { createClient } from '@sanity/client';
 
 dotenv.config();
 
-export const client = createClient({
+const client = createClient({
 	projectId: process.env.SANITY_STUDIO_PROJECT_ID || '',
 	dataset: process.env.SANITY_STUDIO_DATASET || 'production',
 	useCdn: false,
