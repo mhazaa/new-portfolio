d0ce9d361d768b4f681e7bf012eec662099d95ee
mhazaa <magdihazaa@gmail.com>
2024-05-12 04:03:40 -0500

    removing delete functionality through sanity api

--- backend/sanityControls/index.ts
+++ backend/sanityControls/index.ts
@@ -8,15 +8,9 @@ const client = createClient({
 	dataset: process.env.SANITY_STUDIO_DATASET || 'production',
 	useCdn: false,
 	apiVersion: '2023-05-03',
-	token: process.env.SANITY_SECRET_TOKEN,
 });
 
 export const fetch = async (QUERY: string) => {
 	const data = await client.fetch(QUERY);
 	return data;
-};
-
-export const deleteDocument = async (documentId: string) => {
-	const result = client.delete(documentId);
-	return result;
 };
