afba24a57820da22e08d68ed749e62abcce8b7c6
mhazaa <magdihazaa@gmail.com>
2025-02-22 06:53:39 -0500

    adding semicolons and removing unused import from sanity portfolio.ts

--- sanity/schemas/portfolio.ts
+++ sanity/schemas/portfolio.ts
@@ -1,52 +1,50 @@
-import post from './post';
-
 export default {
-  title: 'Portfolio',
-  name: 'portfolio',
+	title: 'Portfolio',
+	name: 'portfolio',
 	type: 'document',
-  fields: [
-    {
-      title: 'Artist',
-      name: 'artist',
-      type: 'array',
-      of: [
-        {
-          type: 'reference',
-          to: [
-            {
-              type: 'post',
-            },
-          ],
-          options: {
-            filter: `category == 'artist'`,
-          }, 
-        },
-      ],
-    },
-    {
-      title: 'Writer',
-      name: 'writer',
-      type: 'array',
-      of: [
-        {
-          type: 'reference',
-          to: [
-            {
-              type: 'post',
-            },
-          ],
-          options: {
-            filter: `category == 'writer'`,
-          },        
-        },
-      ],
-    },
-  ],
-  preview: {
+	fields: [
+		{
+			title: 'Artist',
+			name: 'artist',
+			type: 'array',
+			of: [
+				{
+					type: 'reference',
+					to: [
+						{
+							type: 'post',
+						},
+					],
+					options: {
+						filter: `category == 'artist'`,
+					},
+				},
+			],
+		},
+		{
+			title: 'Writer',
+			name: 'writer',
+			type: 'array',
+			of: [
+				{
+					type: 'reference',
+					to: [
+						{
+							type: 'post',
+						},
+					],
+					options: {
+						filter: `category == 'writer'`,
+					},        
+				},
+			],
+		},
+	],
+	preview: {
 		prepare () {
 			return {
 				title: 'Portfolio',
-			}
+			};
 		},
 	},
-}
+};
