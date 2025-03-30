bdbb8b74f73090c5a932a0de05aaf13928e4cdb0
mhazaa <magdihazaa@gmail.com>
2025-03-30 13:20:59 -0400

    cursor fix

--- frontend/src/components/Cursor.tsx
+++ frontend/src/components/Cursor.tsx
@@ -32,6 +32,7 @@ export const Pointer: React.FC<PointerProps> = ({
 			style={style}
 			onMouseEnter={active ? () => setCursorMode('pointer') : () => {}}
 			onMouseLeave={() => setCursorMode('auto')}
+			onClick={() => setCursorMode('auto')}
 		>
 			{children}
 		</div>
